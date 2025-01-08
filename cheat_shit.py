b18ecb6b4b4fc2e793a28151a381f1354ee15d81
paul decroux <117047767+malayaan@users.noreply.github.com>
2025-01-08 19:36:51 +0100

    Update cheat_shit.py

--- cheat_shit.py
+++ cheat_shit.py
@@ -1,17 +1,8 @@
-# Associer les features aux champs originaux
-feature_scores['Original_Fields'] = feature_scores['Feature'].map(dico)
-
-# Associer les contrôles aux champs originaux
-# On suppose que `control_to_columns` est un dictionnaire similaire pour les contrôles
-control_to_columns = {
-    'control_1': ['field_1', 'field_3'],
-    'control_2': ['field_4', 'field_5'],
-    # Ajouter vos correspondances pour les contrôles ici
-}
-control_scores['Original_Fields'] = control_scores['Control'].map(control_to_columns)
-
-# Initialiser un dictionnaire pour les champs des features
+# Initialiser un dictionnaire pour les champs issus des features
 fields_from_features = {}
+
+# Initialiser un dictionnaire pour les champs issus des contrôles
+fields_from_controls = {}
 
 # Parcourir chaque feature et ses champs associés
 for feature, fields in dico.items():
@@ -23,20 +14,8 @@ for feature, fields in dico.items():
             fields_from_features[field]['Total_Impact_Score'] += impact_score
             fields_from_features[field]['Feature_Count'] += 1
 
-# Transformer en DataFrame
-fields_features_df = pd.DataFrame.from_dict(fields_from_features, orient='index')
-fields_features_df.index.name = 'Field'
-fields_features_df.reset_index(inplace=True)
-fields_features_df['Average_Impact_Score'] = fields_features_df['Total_Impact_Score'] / fields_features_df['Feature_Count']
-
-# Trier par impact total décroissant
-fields_features_df = fields_features_df.sort_values(by='Total_Impact_Score', ascending=False)
-
-# Initialiser un dictionnaire pour les champs des contrôles
-fields_from_controls = {}
-
 # Parcourir chaque contrôle et ses champs associés
-for control, fields in control_to_columns.items():
+for control, fields in dico.items():
     if control in control_scores['Control'].values:  # Vérifier que le contrôle est problématique
         impact_score = control_scores.loc[control_scores['Control'] == control, 'Impact_Score'].values[0]
         for field in fields:
@@ -45,19 +24,27 @@ for control, fields in control_to_columns.items():
             fields_from_controls[field]['Total_Impact_Score'] += impact_score
             fields_from_controls[field]['Control_Count'] += 1
 
-# Transformer en DataFrame
+# Convertir le dictionnaire des features en DataFrame
+fields_features_df = pd.DataFrame.from_dict(fields_from_features, orient='index')
+fields_features_df.index.name = 'Field'
+fields_features_df.reset_index(inplace=True)
+fields_features_df['Average_Impact_Score'] = fields_features_df['Total_Impact_Score'] / fields_features_df['Feature_Count']
+
+# Trier les champs des features par impact total décroissant
+fields_features_df = fields_features_df.sort_values(by='Total_Impact_Score', ascending=False)
+
+# Convertir le dictionnaire des contrôles en DataFrame
 fields_controls_df = pd.DataFrame.from_dict(fields_from_controls, orient='index')
 fields_controls_df.index.name = 'Field'
 fields_controls_df.reset_index(inplace=True)
 fields_controls_df['Average_Impact_Score'] = fields_controls_df['Total_Impact_Score'] / fields_controls_df['Control_Count']
 
-# Trier par impact total décroissant
+# Trier les champs des contrôles par impact total décroissant
 fields_controls_df = fields_controls_df.sort_values(by='Total_Impact_Score', ascending=False)
-
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-# Top 10 champs d'après les features
+# Visualiser les champs problématiques d’après les features
 plt.figure(figsize=(12, 6))
 sns.barplot(
     data=fields_features_df.head(10),
@@ -70,7 +57,7 @@ plt.xlabel("Total Impact Score")
 plt.ylabel("Field")
 plt.show()
 
-# Top 10 champs d'après les contrôles
+# Visualiser les champs problématiques d’après les contrôles
 plt.figure(figsize=(12, 6))
 sns.barplot(
     data=fields_controls_df.head(10),
