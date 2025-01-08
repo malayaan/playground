acd5db6b9d57b5dcfa883bd8585e755d7a784c27
paul decroux <117047767+malayaan@users.noreply.github.com>
2025-01-08 19:41:00 +0100

    Update cheat_shit.py

--- cheat_shit.py
+++ cheat_shit.py
@@ -1,3 +1,3 @@
-# Afficher les champs problématiques d’après les features
-print("\nTop 10 Champs les Plus Problématiques (Basé sur les Features):")
-print(fields_features_df[['Field', 'Total_Impact_Score', 'Feature_Count', 'Average_Impact_Score']].head(10).to_string(index=False))
+# Afficher les champs problématiques d’après les contrôles
+print("\nTop 10 Champs les Plus Problématiques (Basé sur les Contrôles):")
+print(fields_controls_df[['Field', 'Total_Impact_Score', 'Control_Count', 'Average_Impact_Score']].head(10).to_string(index=False))
