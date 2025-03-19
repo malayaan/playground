edff00569d6230c7f1178385239b35039d55af66
paul decroux <117047767+malayaan@users.noreply.github.com>
2025-03-19 22:51:09 +0100

    Update cheat_shit.py

--- cheat_shit.py
+++ cheat_shit.py
@@ -1,16 +1,2 @@
-from tqdm import tqdm
-import optuna
-
-def run_optimization(self):
-    study = optuna.create_study(direction="maximize")
-    
-    # Utiliser tqdm pour suivre la progression des essais
-    with tqdm(total=self.n_trials, desc="Optimisation en cours") as pbar:
-        def callback(study, trial):
-            pbar.update(1)  # Mise à jour de la barre de progression
-        
-        study.optimize(self.objective, n_trials=self.n_trials, callbacks=[callback])
-    
-    self.best_parameters_ = study.best_params
-    self.best_metric_value = study.best_value
-    return study
+conda create -n rapids_env -c rapidsai -c nvidia -c conda-forge -c defaults rapids=23.06 python=3.9 cudatoolkit=11.8
+conda activate rapids_env
