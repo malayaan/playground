f5643f9c1e834b4845e454a90be6fb8d9a76e4e3
paul decroux <117047767+malayaan@users.noreply.github.com>
2025-03-19 22:52:36 +0100

    Update cheat_shit.py

--- cheat_shit.py
+++ cheat_shit.py
@@ -1,2 +1,1 @@
-conda create -n rapids_env -c rapidsai -c nvidia -c conda-forge -c defaults rapids=23.06 python=3.9 cudatoolkit=11.8
-conda activate rapids_env
+conda install -c rapidsai -c nvidia -c conda-forge -c defaults cuml=23.06 python=3.9 cudatoolkit=11.8
