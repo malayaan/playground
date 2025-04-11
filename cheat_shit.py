334e49c94f7f43c0af6d6cf5aadb3fea3ed7ac4d
paul decroux <117047767+malayaan@users.noreply.github.com>
2025-04-11 10:13:51 +0200

    Update cheat_shit.py

--- cheat_shit.py
+++ cheat_shit.py
@@ -1,1 +1,1 @@
-Ces résultats permettent d’isoler les cas les plus longs et d’orienter des actions concrètes d’optimisation ou d’automatisation
+Ces résultats permettent d’isoler les appels les plus longs et d’ajuster le pilotage des pratiques des conseillers sur la gestion des temps d’attente
