592a8292f708b75392bac793efcc6cff37ae54b1
malayaan <117047767+malayaan@users.noreply.github.com>
2025-05-16 17:58:11 +0200

    modified:   cheat_shit.py 	new file:   indices_sectoriels_base100.xlsx 	new file:   indices_sectoriels_janv_mars_2025.csv

--- cheat_shit.py
+++ cheat_shit.py
@@ -1,15 +1,35 @@
-import seaborn as sns
-import requests
-import pandas as pd
-import matplotlib.pyplot as plt
-import time
-import re
-import numpy as np
-import time
-from datetime import datetime
-import plotly.express as px
-import plotly.figure_factory as ff
-pd.set_option("display.max_rows", None)
-pd.set_option('display.max_columns', None)
-pd.set_option('display.max_colwidth', None)
-pd.set_option('expand_frame_repr', False)
+❌ Erreur pour STOXX Europe 600 Food & Beverage (^STOXX600F&B): 'NoneType' object is not subscriptable
+❌ Erreur pour Euronext Food & Beverage Index (^FBEURONEXT): 'NoneType' object is not subscriptable
+✅ S&P 500 Consumer Staples téléchargé
+❌ Erreur pour NASDAQ Consumer Staples (^IXY): 'timestamp'
+❌ Erreur pour STOXX Europe 600 Automobiles & Parts (SXAP.SW): 'NoneType' object is not subscriptable
+✅ S&P 500 Automobiles & Components téléchargé
+❌ Erreur pour Dow Jones Automobiles & Parts (DJUSAP): 'NoneType' object is not subscriptable
+❌ Erreur pour STOXX Europe 600 Oil & Gas (SXEP.SW): 'NoneType' object is not subscriptable
+❌ Erreur pour S&P 500 Energy (^SP500-10): 'NoneType' object is not subscriptable
+✅ S&P 500 Utilities téléchargé
+❌ Erreur pour STOXX Europe 600 Utilities (SX6P.SW): 'NoneType' object is not subscriptable
+❌ Erreur pour STOXX Europe 600 Industrial Goods & Services (SXNP.SW): 'NoneType' object is not subscriptable
+❌ Erreur pour STOXX Europe 600 Chemicals (SX4P.SW): 'NoneType' object is not subscriptable
+❌ Erreur pour FTSE 350 Industrial Metals & Mining (^FT350IMM): 'NoneType' object is not subscriptable
+✅ S&P 500 Industrials téléchargé
+❌ Erreur pour Dow Jones U.S. Basic Materials (DJUSBM): 'NoneType' object is not subscriptable
+❌ Erreur pour STOXX Europe 600 Technology (SX8P.SW): 'NoneType' object is not subscriptable
+❌ Erreur pour STOXX Europe 600 Telecommunications (SXKP.SW): 'NoneType' object is not subscriptable
+❌ Erreur pour FTSE TechMARK 100 (^FTTM100): 'NoneType' object is not subscriptable
+✅ NASDAQ Composite téléchargé
+✅ S&P 500 Information Technology téléchargé
+❌ Erreur pour Dow Jones U.S. Telecommunications (DJUSTL): 'NoneType' object is not subscriptable
+❌ Erreur pour STOXX Europe 600 Health Care (SXDP.SW): 'NoneType' object is not subscriptable
+❌ Erreur pour FTSE 350 Pharmaceuticals & Biotechnology (^FT350PB): 'NoneType' object is not subscriptable
+✅ S&P 500 Health Care téléchargé
+✅ NASDAQ Biotechnology téléchargé
+❌ Erreur pour FTSE 350 Industrial Transportation (^FT350IT): 'NoneType' object is not subscriptable
+✅ Dow Jones Transportation Average téléchargé
+❌ Erreur pour STOXX Europe 600 Retail (SXRP.SW): 'NoneType' object is not subscriptable
+❌ Erreur pour FTSE 100 Consumer Discretionary (^FT100CD): 'NoneType' object is not subscriptable
+✅ S&P 500 Consumer Discretionary téléchargé
+❌ Erreur pour NASDAQ Retail Trade Index (^RETAIL): 'NoneType' object is not subscriptable
+❌ Erreur pour STOXX Europe 600 Construction & Materials (SXOP.SW): 'NoneType' object is not subscriptable
+✅ S&P 500 Materials téléchargé
+❌ Erreur pour Dow Jones U.S. Construction & Materials (DJUSCN): 'NoneType' object is not subscriptable
