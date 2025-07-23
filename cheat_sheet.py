09f32a542d2a1a7ecb88f4ed3bffde5d54c1db3b
paul decroux <117047767+malayaan@users.noreply.github.com>
2025-07-23 16:51:28 +0200

    Update cheat_sheet.py

--- cheat_sheet.py
+++ cheat_sheet.py
@@ -1,25 +1,10 @@
-fig.update_layout(
-    title="Skill Level Distribution",
-    xaxis=dict(
-        title="Team",
-        tickvals=df["audit_team_l3"].unique(),
-        ticktext=df["X_labels"],
-        showline=True,
-        linewidth=1,
-        zeroline=False,
-        font=dict(weight='bold')  # Ajoutez cette ligne pour mettre les labels X en gras
-    ),
-    yaxis=dict(
-        title="Percentage (%)",
-        range=[0, 110],
-        showgrid=False,
-        zeroline=False,
-        showline=True,
-        linewidth=1,
-    ),
-    height=600,
-    barmode="group",  # Vous pouvez aussi ajouter la couleur de fond ici
-    paper_bgcolor="white"
+st.markdown(
+    """
+    <style>
+    .css-1d391kg {
+        margin-top: -50px !important;
+    }
+    </style>
+    """, 
+    unsafe_allow_html=True
 )
-
-st.plotly_chart(fig, use_container_width=True)
