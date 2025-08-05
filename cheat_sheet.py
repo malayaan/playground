16a2677e4b7bf2aa3a430b2b80a91c92fd9e4a4f
paul decroux <117047767+malayaan@users.noreply.github.com>
2025-08-05 11:54:44 +0200

    Update cheat_sheet.py

--- cheat_sheet.py
+++ cheat_sheet.py
@@ -1,10 +1,44 @@
-st.markdown(
-    """
-    <style>
-    .css-1d391kg {
-        margin-top: -50px !important;
-    }
-    </style>
-    """, 
-    unsafe_allow_html=True
-)
+2015	Q1	29 avril 2015
+	Q2	29 juillet 2015
+	Q3	28 octobre 2015
+	Q4	10 mars 2016
+2016	Q1	28 avril 2016
+	Q2	29 juillet 2016
+	Q3	27 octobre 2016
+	Q4	28 mars 2017
+2017	Q1	27 avril 2017
+	Q2	27 juillet 2017
+	Q3	26 octobre 2017
+	Q4	27 mars 2018
+2018	Q1	26 avril 2018
+	Q2	26 juillet 2018
+	Q3	25 octobre 2018
+	Q4	26 mars 2019
+2019	Q1	25 avril 2019
+	Q2	25 juillet 2019
+	Q3	24 octobre 2019
+	Q4	25 mars 2020
+2020	Q1	29 avril 2020
+	Q2	29 juillet 2020
+	Q3	28 octobre 2020
+	Q4	25 mars 2021
+2021	Q1	29 avril 2021
+	Q2	29 juillet 2021
+	Q3	28 octobre 2021
+	Q4	25 mars 2022
+2022	Q1	28 avril 2022
+	Q2	28 juillet 2022
+	Q3	27 octobre 2022
+	Q4	28 mars 2023
+2023	Q1	27 avril 2023
+	Q2	27 juillet 2023
+	Q3	26 octobre 2023
+	Q4	27 mars 2024
+2024	Q1	29 avril 2024
+	Q2	29 juillet 2024
+	Q3	28 octobre 2024
+	Q4	25 mars 2025
+2025	Q1	30 avril 2025
+	Q2	25 juillet 2025
+	Q3	29 octobre 2025
+	Q4	16 mars 2026
